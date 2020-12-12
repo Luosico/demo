aea539edad4111c5be774c82c85dda13fe2c5700
luokaifa <1142445138@qq.com>
2020-12-12 14:59:15 +0800

    update

--- spring-cloud/spring-cloud-provider/src/main/java/com/luosico/springcloudprovider/SpringCloudProviderApplication.java
+++ spring-cloud/spring-cloud-provider/src/main/java/com/luosico/springcloudprovider/SpringCloudProviderApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudProviderApplication {
+public class  SpringCloudProviderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudProviderApplication.class, args);
