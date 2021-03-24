edf603a66d184806d9897e0d2129a4195458ec9c
luosico <lootalker@gmail.com>
2021-03-24 12:27:52 +0800

    使用 Nacos 作为配置中心

--- spring-cloud/spring-cloud-consumer/src/main/java/com/luosico/demo/DemoController.java
+++ spring-cloud/spring-cloud-consumer/src/main/java/com/luosico/demo/DemoController.java
@@ -3,10 +3,14 @@ package com.luosico.demo;
 
 import com.luosico.UserService;
 import org.apache.dubbo.config.annotation.DubboReference;
-//import org.apache.dubbo.rpc.RpcContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,14 +22,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 
 @Controller
+@RefreshScope
 public class DemoController {
 
 
     @DubboReference
     UserService userService;    //生成Dubbo调用服务提供者对象
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     //日志对象
     private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @Value("${info:default value}")
+    private String info;
 
     @RequestMapping(value = "/userService")
     @ResponseBody
@@ -44,5 +55,18 @@ public class DemoController {
     public String init() {
 
         return "hello";
+    }
+
+    @GetMapping("/config")
+    @ResponseBody
+    public String nacosConfig(){
+        return "info: " + info;
+    }
+
+    @GetMapping("/redis")
+    @ResponseBody
+    public String testRedisConfig(){
+        redisTemplate.opsForValue().set("redis","success");
+        return "try";
     }
 }
